4682e532300f20a9c87898df12e6262ec63be85b
Jard Krm <krjared@gmail.com>
2021-07-10 11:17:30 -0500

    display high score and level

--- scoreboard.py
+++ scoreboard.py
@@ -16,6 +16,8 @@ class Scoreboard:
 
         # Prepare the initial score image.
         self.prep_score()
+        self.prep_high_score()
+        self.prep_level()
 
     def prep_score(self):
         """Turn the score into a rendered image."""
@@ -29,6 +31,37 @@ class Scoreboard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_high_score(self):
+        """Turn high score into a rendered image."""
+        high_score = round(self.stats.high_score)
+        high_score_string = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_string, True,
+            self.text_color, self.settings.bg_color)
+
+        # Display high score at the top center of the screen.
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.screen_rect.top 
+
+    def prep_level(self):
+        """Turn the level into a rendered image."""
+        level = self.stats.level
+        level_str = str(level)
+        self.level_image = self.font.render(level_str, True, 
+            self.text_color, self.settings.bg_color)
+
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+
     def show_score(self):
         """Draw score to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
+
+    def check_high_score(self):
+        """Check if score is higher then high score."""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
